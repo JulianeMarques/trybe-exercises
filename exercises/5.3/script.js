38669de73eb62bf2f9be2bea48faa07356218e45
Juliane Marques <mqs.juliane@gmail.com>
2021-05-22 15:44:08 -0300

    adiciona ex 9

--- exercises/5.3/script.js
+++ exercises/5.3/script.js
@@ -150,3 +150,18 @@ function createHolidayButton(buttonName) {
   };
   
   newTaskDiv('green');
+
+  function setTaskClass() {
+    let selectedTask = document.getElementsByClassName('task selected');
+    let myTasks = document.querySelector('.task');
+  
+    myTasks.addEventListener('click', function(event) {
+      if (selectedTask.length === 0) {
+        event.target.className = 'task selected';
+      } else {
+        event.target.className = 'task';
+      }
+    });
+  };
+  
+  setTaskClass();
