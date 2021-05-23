d92dd7fa55b6b221606e089f320d075545e87a69
Juliane Marques <mqs.juliane@gmail.com>
2021-05-23 14:16:55 -0300

    adiciona exercicio

---
+++ exercises/5.4/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    let select = document.querySelector('select');
+    select.addEventListener('change', function(){
+        let selected = select.selectedOptions[0];
+        document.body.style.backgroundColor = select.value;
+    })
+
+    let inputFontSize = document.querySelector('input[name="font-size"]');
+    inputFontSize.addEventListener('change', function(){
+        let p = document.querySelector('p');
+        p.style.fontSize = `${inputFontSize.value}px`;
+    })
+}
