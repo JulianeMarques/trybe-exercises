626ccdbfe2a3c528e53ac1b4a58122157f6a87cb
Juliane Marques <mqs.juliane@gmail.com>
2021-05-26 15:57:33 -0300

    fazendo algumas adicoes ao github

---
+++ exercises/7.1/script01.js
@@ -0,0 +1,15 @@
+// exercicio 1
+
+testingScope = (escopo) => {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+
+  }
+
+  testingScope(true);
