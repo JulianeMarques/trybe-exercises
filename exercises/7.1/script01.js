d594f2d3df35445a5c6985f0fa352090a1039d3c
Juliane Marques <mqs.juliane@gmail.com>
2021-05-30 14:43:42 -0300

    adiciona 7.1 part 2 exerc 2

--- exercises/7.1/script01.js
+++ exercises/7.1/script01.js
@@ -60,3 +60,19 @@ const factorial = number => {
 console.log(factorial(4))
 
 // exercicio 2
+
+const longestWord = text => {
+  let wordArray = text.split(' ')
+  let maxLength = 0
+  let result = ''
+
+  for (const word of wordArray) {
+    if(word.length > maxLength) {
+      maxLength = word.length
+      result = word
+    }
+  }
+  return result
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu xablaublaublau"))
