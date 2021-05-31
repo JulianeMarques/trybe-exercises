7df5566e50bfc807f5afaf58e2909886f09f180b
Juliane Marques <mqs.juliane@gmail.com>
2021-05-31 12:01:05 -0300

    adiciona exercicio 2

--- exercises/7.2/parte1.js
+++ exercises/7.2/parte1.js
@@ -36,15 +36,24 @@ const order = {
   
   const customerInfo = (order) => {
     // Adicione abaixo as informações necessárias.
-    console.log(`Ola ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, ${order.phoneNumber}, ${order.address.street}, No ${order.address.number}, AP ${order.address.apartment}`)
+    //console.log(`Ola ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, ${order.phoneNumber}, ${order.address.street}, No ${order.address.number}, AP ${order.address.apartment}`)
   }
   
   // "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
-  customerInfo(order);
+  //customerInfo(order);
   
   const orderModifier = (order) => {
     // Adicione abaixo as informações necessárias.
-  
+    const newBuyer = order.name = 'Luiz Silva';
+    const pizzas = Object.keys(order.order.pizza);
+    const drink = order.order.drinks.coke.type;
+    const newTotal = order.payment.total = 50;
+
+    console.log(`Ola ${newBuyer}, o total do seu pedido de ${pizzas} e ${drink} e de ${newTotal}`)
+
   }
-  
+  /* "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
+Modifique o nome da pessoa compradora.
+Modifique o valor total da compra para R$ 50,00. */
+
   orderModifier(order);
