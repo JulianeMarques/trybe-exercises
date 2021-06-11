e964025c905563d339bfa19fd7bd68b37a30afb0
Juliane Marques <mqs.juliane@gmail.com>
2021-06-11 17:18:48 -0300

    exercicio entrega

--- exercises/7.2/parte1.js
+++ exercises/7.2/parte1.js
@@ -1,59 +1,67 @@
 const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
-    },
-    order: {
-      pizza: {
-        margherita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      margherita: {
+        amount: 1,
+        price: 25,
       },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
-      },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
+      pepperoni: {
+        amount: 1,
+        price: 20,
       }
     },
-    payment: {
-      total: 60,
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
     },
-  };
-  
-  const customerInfo = (order) => {
-    // Adicione abaixo as informações necessárias.
-    //console.log(`Ola ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, ${order.phoneNumber}, ${order.address.street}, No ${order.address.number}, AP ${order.address.apartment}`)
-  }
-  
-  // "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
-  //customerInfo(order);
-  
-  const orderModifier = (order) => {
-    // Adicione abaixo as informações necessárias.
-    const newBuyer = order.name = 'Luiz Silva';
-    const pizzas = Object.keys(order.order.pizza);
-    const drink = order.order.drinks.coke.type;
-    const newTotal = order.payment.total = 50;
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
 
-    console.log(`Ola ${newBuyer}, o total do seu pedido de ${pizzas} e ${drink} e de ${newTotal}`)
+const customerInfo = (order) => {
+  const address = 'address';
+  const deliveryPerson = order.order.delivery.deliveryPerson;
+  const customerName = order['name'];
+  const customerPhone = order['phoneNumber'];
+  const street = order[address].street;
+  const number = order[address].number;
+  const apartment = order[address].apartment;
 
-  }
-  /* "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
-Modifique o nome da pessoa compradora.
-Modifique o valor total da compra para R$ 50,00. */
+  console.log(`Olá ${deliveryPerson}, entrega para: ${customerName}, Telefone: ${customerPhone}, R. ${street}, Nº: ${number}, AP: ${apartment}`);
+}
 
-  orderModifier(order);
+customerInfo(order);
+
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+}
+
+
+const orderModifier = (order) => {
+  const newBuyer = order.name = 'Luiz Silva';
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = order.order.drinks.coke.type;
+  const newTotal = order.payment.total = '50';
+
+  console.log(`Olá ${newBuyer}, o total do seu pedido de ${pizzas} e ${drinks} é R$ ${newTotal},00.`);
+}
+
+orderModifier(order);
