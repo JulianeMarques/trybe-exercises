231a703e414b8f2bf6e25e80d8e52477ed6d8d6a
Juliane Marques <mqs.juliane@gmail.com>
2021-07-07 18:19:09 -0300

    adiciona exercicios 9.2

--- exercises/9.2/script.js
+++ exercises/9.2/script.js
@@ -1,8 +1,32 @@
-// apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
-};
+  fetch(API_URL, { headers: { Accept: 'application/json' } })
+    .then(response => response.json())
+    .then(data =>
+      document.getElementById('jokeContainer').innerText = data.joke
+    );
+}
 
 window.onload = () => fetchJoke();
+
+// - ex 2 
+
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const myArray = Array.from(
+      { length: 10 },
+      () =>  Math.floor(Math.random() * 50) + 1
+    );
+    const sum = myArray.map(number => number * number)
+                       .reduce((sum, number) => sum + number);
+
+    (sum < 8000) ? resolve(sum) : reject(sum);
+  });
+
+  myPromise
+    .then((sum) => console.log(`'Promise resolvida' ${sum}`))
+    .catch((sum) => console.log(`'É mais de oito mil! Essa promise deve estar quebrada!' ${sum}`));
+};
+
+fetchPromise();
