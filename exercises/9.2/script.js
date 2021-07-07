ab97161069aefc92a736d7ee57c39233829ede5a
Juliane Marques <mqs.juliane@gmail.com>
2021-07-07 19:02:20 -0300

    adiciona bonus

--- exercises/9.2/script.js
+++ exercises/9.2/script.js
@@ -25,8 +25,39 @@ const fetchPromise = () => {
   });
 
   myPromise
-    .then((sum) => console.log(`'Promise resolvida' ${sum}`))
+    .then((sum) => [2, 3, 5, 10].map(number => sum / number))
+    .then(array => array.reduce((number, acc) => number + acc, 0))
     .catch((sum) => console.log(`'É mais de oito mil! Essa promise deve estar quebrada!' ${sum}`));
 };
+
+fetchPromise();
+
+// --- bonus
+
+const sumRandomNumbers = () => {
+  const myArray = Array.from(
+    { length: 10 },
+    () => Math.floor(Math.random() * 50) + 1
+  );
+  const sum = myArray.map(number => number * number)
+  .reduce((number, acc) => number + acc, 0);
+
+  if (sum >= 8000) {
+    throw new Error();
+  }
+
+  return sum;
+}
+
+const sumArrayFromSum = (sum) => [2, 3, 5, 10].map(number => sum / number)
+.reduce((number, acc) => number + acc);
+
+const fetchPromise = async () => {
+  try {
+    const sum = await sumArrayFromSum(sum);
+  } catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!');
+  }
+}
 
 fetchPromise();
